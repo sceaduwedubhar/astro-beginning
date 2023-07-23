6665d0009fcb5fcc8a94aa7f4f627ba65054a0ae
Morris <>
2023-07-23 10:31:54 -0400

    pagination update

--- src/components/ui/PaginationReact.tsx
+++ src/components/ui/PaginationReact.tsx
@@ -2,42 +2,31 @@ import { useEffect, useState } from "react";
 
 export function PaginationReact({ total, emitFn }: { total: number; emitFn: Function }) {
     const [current, useCurrent] = useState(1);
-    const [first, useFirst] = useState(false);
-    const [end, useEnd] = useState(false);
 
     useEffect(() => {
         useCurrent(1);
     }, [total]);
 
     useEffect(() => {
-        useFirst(false);
-        useEnd(false);
-        if (current <= 1) {
-            useFirst(true);
-        }
-        if (current >= total) {
-            useEnd(true);
-        }
+        emitFn(current);
     }, [current]);
 
     function nextPage() {
         if (current >= total) return;
         useCurrent((prev) => prev + 1);
-        emitFn(current);
     }
 
     function prevPage() {
         if (current <= 1) return;
         useCurrent((prev) => prev - 1);
-        emitFn(current);
     }
 
     return (
         <div className="flex gap-1 justify-center">
-            <div className={`p-2 ${first ? "text-gray-400" : "cursor-pointer"}`} onClick={prevPage}>
+            <div className={`p-2 ${current <= 1 ? "text-gray-400" : "cursor-pointer"}`} onClick={prevPage}>
                 <i className={`fa-solid fa-caret-left text-2xl`}></i>
             </div>
-            <div className={`p-2 ${end ? "text-gray-400" : "cursor-pointer"}`} onClick={nextPage}>
+            <div className={`p-2 ${current >= total ? "text-gray-400" : "cursor-pointer"}`} onClick={nextPage}>
                 <i className={`fa-solid fa-caret-right text-2xl`}></i>
             </div>
         </div>
