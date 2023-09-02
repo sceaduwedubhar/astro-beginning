5d6c3b6453af1bf1e2a777456dd545c213ab1f5f
Morris <>
2023-09-02 00:58:25 -0400

    update

--- src/components/novelList/NovelShelf.tsx
+++ src/components/novelList/NovelShelf.tsx
@@ -5,7 +5,11 @@ export function NovelShelf({ data, removeFn }: { data: NovelData[]; removeFn: Fu
         <div className="grid grid-cols-3 gap-1">
             {data.map((v: NovelData) => (
                 <div className="grid border rounded-lg p-2 gap-1 bg-gray-300">
-                    <div>Title: {v.title}</div>
+                    <div>
+                        <a href={`https://ncode.syosetu.com/${v.ncode.toLowerCase()}/`} target="_blank">
+                            Title: {v.title}
+                        </a>
+                    </div>
                     <div>Author: {v.writer}</div>
                     <div className="flex justify-between">
                         <div>{STATE[v.end]}</div>
